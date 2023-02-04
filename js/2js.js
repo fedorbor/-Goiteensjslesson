514ad1ff9d165fb58a0736d0324cdaf8910cf008
fedorbor <112242236+fedorbor@users.noreply.github.com>
2023-02-04 12:58:04 +0200

    d

--- js/2js.js
+++ js/2js.js
@@ -1,21 +1,31 @@
-const a ='50px';
-const b = Number.parseInt(a);
-console.log(a);
-console.log(Number.parseInt(a));
-const x ='50.27px';
-const y = Number.parseFloat(x);
-console.log(x);
-console.log(y);
-const c = 232.232;
-const d = c.toFixed(2);
-const e = Number(d);
-console.log(d);
-console.log(e);
+// const cards = [
+//     'Карточка-1',
+//     'Карточка-2',
+//     'Карточка-3',
+//     'Карточка-4',
+//     'Карточка-5',
+//   ];
 
-const firstName = 'Mango';
-const lastName = 'Lee';
-const room = 716;
-const type = 'VIP';
-const userData = "Гість " +firstName+" "+lastName+" поселяється в " +room+" номер "+type;
-console.log(userData)
-// «Гість x y поселяється в g номер q»,
+// /*
+//    * Видалення (по індексу), метод indexOf()
+//    */
+//   const cardToRemove = 'Карточка-3';
+// /*
+//    * Додавання (по індексу)
+//    */
+  
+// const cardToInsert = 'Карточка-6';
+// /*
+//    * Оновлення (по індексу)
+//    */
+//   const cardToUpdate = 'Карточка-4';
+//   const index=cards.indexOf(cardToRemove);
+  
+//   cards.splice(index,1);
+  
+//   const index2=cards.length
+//   cards.splice(index2,0,cardToInsert);
+
+// const index3=cards.indexOf(cardToUpdate)
+// cards.splice(index3,1,"Карточка")
+//   console.log(cards);
